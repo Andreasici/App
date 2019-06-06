69ced8bbf960243a18544387069ae8ab8a2437ef
Nicholas <nicholas.cerrone.99@gmail.com>
2019-06-06 12:44:21 +0200

    LoginActivity.java: first handling of login process and TODO things
    activity_login.xml: layout of Login
    
    Version 0.0.1

--- app/src/main/java/com/example/app/LoginActivity.java
+++ app/src/main/java/com/example/app/LoginActivity.java
@@ -2,12 +2,53 @@ package com.example.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText editEmail;
+    EditText editPassword;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // input's fields
+        editEmail = findViewById(R.id.editEmail);
+        editPassword = findViewById(R.id.editPassword);
+
+        // login button
+        btnLogin = findViewById(R.id.btnLogin);
+
+        // login button listener
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // check login
+                if(checkLogin()){
+                    // login ok
+                    // intent to the next activity
+                }else{
+                    // login wrong
+                    // notice user
+                }
+            }
+        });
+    }
+
+    /*
+        Check here the login info connecting to the web service
+     */
+    private boolean checkLogin(){
+        /* TODO
+        // 1) connecting to the web service as database for account check (email/password)
+        // 2) check whether the input's values match some occurencies on the database
+        // 3a) if match -> open next Activity
+        // 3b) if not match -> notice user about errors
+        */
     }
 }
