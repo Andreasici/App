28a5747d24191b2cdf1a467add88e262c9e1ef37
n.cerrone <nicholas.cerrone.99@gmail.com>
2019-09-30 11:42:51 +0200

    First-map integration, creation-button option and first gestures on the map, basic approach Version 0.0.3

--- app/src/main/java/com/example/routesplanner/FragmentMyMaps.java
+++ app/src/main/java/com/example/routesplanner/FragmentMyMaps.java
@@ -1,11 +1,15 @@
 package com.example.routesplanner;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.routesplanner.activity.NewMapActivity;
 
 
 /**
@@ -13,6 +17,7 @@ import android.view.ViewGroup;
  */
 public class FragmentMyMaps extends Fragment {
 
+    Button createNewMapButton;
 
     public FragmentMyMaps() {
         // Required empty public constructor
@@ -22,8 +27,29 @@ public class FragmentMyMaps extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_my_maps, container, false);
+        View view = inflater.inflate(R.layout.fragment_fragment_my_maps, container, false);
+        //return inflater.inflate(R.layout.fragment_fragment_my_maps, container, false);
+
+        createNewMapButton = (Button) view.findViewById(R.id.createNewMap);
+
+        createNewMapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createNewMap(getView());
+            }
+        });
+
+        return view;
+    }
+
+    // method called when the user wants to create a new map (using the button)
+    public void createNewMap(View view){
+
+        // call another activity and start create a new map
+        Intent intent = new Intent(getActivity(), NewMapActivity.class);
+        startActivity(intent);
     }
 
 }
