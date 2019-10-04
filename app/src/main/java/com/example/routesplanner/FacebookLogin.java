c6e75c2987c8e9ae1ed9abb361be0c3be7520722
a.siciliano <andrea.siciliano@edu.itspiemonte.it>
2019-10-04 16:28:01 +0200

    Delete Facebook button from the login fragment Version 0.0.4

--- app/src/main/java/com/example/routesplanner/FacebookLogin.java
+++ app/src/main/java/com/example/routesplanner/FacebookLogin.java
@@ -1,6 +1,6 @@
 package com.example.routesplanner;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class FacebookLogin extends AppCompatActivity {
--- app/src/main/java/com/example/routesplanner/activity/MainActivity.java
+++ app/src/main/java/com/example/routesplanner/activity/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.routesplanner.activity;
 
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.view.ViewPager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.tabs.TabLayout;
+import androidx.core.view.GravityCompat;
+import androidx.viewpager.widget.ViewPager;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.routesplanner.CustomPageAdapter;
 import com.example.routesplanner.R;
